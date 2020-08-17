b351e3cac121fcd6533630b91b0dfbe658cf10d1
Ian Chen <iancodinghtml@gmail.com>
2020-08-17 20:57:37 +0800

    update class.ts

--- Example/class/class.ts
+++ Example/class/class.ts
@@ -7,5 +7,17 @@ class people {
         return `Hello! My name is ${this.name}`;
     }
 }
-let ian = new people('Ian');
-console.log(ian.sayHi())
+class superHero extends people {
+    public birthday: number;
+    constructor(name: string, birthday: number){
+        super(name);
+        this.birthday = birthday;
+    }
+    tellMeBirthday(){
+        return `${this.birthday}`
+    }
+}
+// let ian = new people('Ian');
+// console.log(ian.sayHi())
+let peter = new superHero('Peter',19990119);
+console.log(peter.tellMeBirthday())
