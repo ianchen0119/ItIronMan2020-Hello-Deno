4d2233d90919cfa131003a70aff320f2e0438c69
ianchen0119 <t106360734@ntut.edu.tw>
2020-09-06 23:26:06 +0800

    [UPDATE] CH5

--- Example/for.ts
+++ Example/for.ts
@@ -1,8 +1,19 @@
-for (var i = 1; i <= 10; i++) {
-  console.log(`${i}隻`);
-}
+// for (var i = 1; i <= 10; i++) {
+//   console.log(`${i}隻`);
+// }
 var i = 1;
 while (i <= 10) {
+  if (i === 3) {
+    i++;
+    continue;
+  }
   console.log(`${i}隻`);
   i++;
 }
+// while (true) {
+//   console.log(`${i}隻`);
+//   if (i === 10) {
+//     break;
+//   }
+//   i++;
+// }
