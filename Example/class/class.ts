497d4f6c84dbc9be270f6ee9035b44c74bf62c14
unknown <iancodinghtml@gmail.com>
2020-10-07 19:01:05 +0800

    [ADD] ch20

--- Example/class/class.ts
+++ Example/class/class.ts
@@ -1,23 +1,24 @@
 class people {
-    public name:string;
-    constructor(name:string){
-        this.name = name;
-    }
-    sayHi(){
-        return `Hello! My name is ${this.name}`;
-    }
+  public name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  sayHi() {
+    return `Hello! My name is ${this.name}`;
+  }
 }
 class superHero extends people {
-    public birthday: number;
-    constructor(name: string, birthday: number){
-        super(name);
-        this.birthday = birthday;
-    }
-    tellMeBirthday(){
-        return `${this.birthday}`
-    }
+  public birthday: number;
+  constructor(name: string, birthday: number) {
+    super(name);
+    this.birthday = birthday;
+  }
+  tellMeBirthday() {
+    return `${this.birthday}`;
+  }
 }
 // let ian = new people('Ian');
 // console.log(ian.sayHi())
-let peter = new superHero('Peter',19990119);
-console.log(peter.tellMeBirthday())
+let peter = new superHero("Peter", 19990119);
+console.log(peter.tellMeBirthday());
+eter.tellMeBirthday())
--- Example/class/classAndInterface.ts
+++ Example/class/classAndInterface.ts
@@ -2,7 +2,7 @@
 //     public name: string;
 //     constructor(name: string) {
 //         this.name = name;
-//     }    
+//     }
 //     sayHi() {
 //         return `My name is ${this.name}`;
 //     }
@@ -14,10 +14,10 @@
 // class Cat extends Animal implements cat{
 //     constructor(name: string) {
 //         super(name); // 呼叫父類別的 constructor(name)
-//         console.log(this.name);    
+//         console.log(this.name);
 //     }
 //     sayHi() {
-//         return 'Meow, ' + super.sayHi(); // 呼叫父類別的 sayHi()    
+//         return 'Meow, ' + super.sayHi(); // 呼叫父類別的 sayHi()
 //     }
 //     sayMeow(){
 //         return 'Meow~';
@@ -26,11 +26,11 @@
 // let c = new Cat('Tom'); // Tom
 // console.log(c.sayHi()); // Meow, My name is Tom
 class Point {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
 
 interface Point3d extends Point {
-    z: number;
+  z: number;
 }
-let point3d: Point3d = {x: 1, y: 2, z: 3};
+let point3d: Point3d = { x: 1, y: 2, z: 3 };
